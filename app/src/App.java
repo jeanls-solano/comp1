3d2f0f03426b67c2ae51e724814688b2fdeaa178
Jean Lucas Solano Fortes <jeanlucaseh10@gmail.com>
2022-07-15 16:16:42 -0400

    iniciando o sintático

--- app/src/App.java
+++ app/src/App.java
@@ -1,14 +1,19 @@
+import java.util.ArrayList;
+
 import assistants.LexScanner;
 import assistants.Token;
 
 public class App {
 
+    static ArrayList<Token> entrada = new ArrayList<Token>();
     public static void main(String[] args) {
         LexScanner scan = new LexScanner("input.txt");
         Token token = null;
         do {
             token = scan.nextToken();
+            entrada.add(token);
             System.out.println(token);
         } while (token != null);
+
     }
 }
--- app/src/assistants/SyntheticAnalytic.java
+++ app/src/assistants/SyntheticAnalytic.java
@@ -4,21 +4,75 @@ import java.util.ArrayList;
 
 public class SyntheticAnalytic {
 
-    ArrayList<String> acao = new ArrayList<String>();
+    //ação vai ser (case e if) 
     ArrayList<String> entrada = new ArrayList<String>();
     static ArrayList<String> simbolo = new ArrayList<String>();
     static ArrayList<Integer> estado = new ArrayList<Integer>();
 
     public static void main(String[] args) {
         estado.add(0);
-        estado.add(5);
-        estado.remove(0);
-        System.out.println("Último elemento da lista: " + estado.get(estado.size()-1));
+        // só testando para adicioar o estado
+        // estado.add(5);
+        // estado.remove(0);
+        // System.out.println("Último elemento da lista: " + estado.get(estado.size()-1));
     }
+
+    /*  
+    1 = E -> E + T
+    2 = E -> E - T  
+    3 = E -> T
+    4 = T -> T * P 
+    5 = T -> T / P
+    6 = T -> P
+    7 = P ->  P ^ F
+    8 = P -> exp [ F ] 
+    9 = P -> F
+    10 = F -> ( E )
+    11 = F -> id
+    */
+
+
+
 
     if(entrada=="+"){
-        // + get()
-        switch (estado.get()) {
+        // pega o ultimo elemento do estado
+        switch (estado.get( estado.size()-1)) {
+            case 1:
+                estado.add(8);
+                // simbolo recebe a entrada 
+                simbolo.add(entrada.get(entrada.size()-1));
+                // remove o ultimo elemento da entrada
+                entrada.remove( entrada.size()-1);
+            case 5:
+                //9 = P -> F
+                //desempilha o ultimo simbolo
+                simbolo.remove(simbolo.size()-1);
+                //subistitui pela redução
+                simbolo.add("P");
+                // desempilha o estado atual
+                estado.remove(estado.size()-1);
+            case 7:
+
+            case 14:
+
+            case 15:
+
+            case 16:
+
+            case 17:
+
+            case 18:
+
+            case 19:
+
+            case 21:
+
+            case 22:
+
+        }
+    }
+    if(entrada=="-"){
+        switch (estado.get(estado.get( estado.size()-1))) {
             case 1:
 
             case 5:
@@ -43,38 +97,9 @@ public class SyntheticAnalytic {
 
         }
     }
-    if(entrada=="-")
-    {
+    if(entrada=="*"){
         // + get()
-        switch (estado.get()) {
-            case 1:
-
-            case 5:
-
-            case 7:
-
-            case 14:
-
-            case 15:
-
-            case 16:
-
-            case 17:
-
-            case 18:
-
-            case 19:
-
-            case 21:
-
-            case 22:
-
-        }
-    }
-    if(entrada=="*")
-    {
-        // + get()
-        switch (estado.get()) {
+        switch (estado.get(estado.get( estado.size()-1))) {
             case 2:
 
             case 5:
@@ -99,8 +124,8 @@ public class SyntheticAnalytic {
     }
     if(entrada=="/")
     {
-        // + get()
-        switch (estado.get()) {
+        // + get(estado.get( estado.size()-1))
+        switch (estado.get(estado.get( estado.size()-1))) {
             case 2:
 
             case 5:
@@ -125,8 +150,8 @@ public class SyntheticAnalytic {
     }
     if(entrada=="^")
     {
-        // + get()
-        switch (estado.get()) {
+        // + get(estado.get( estado.size()-1))
+        switch (estado.get(estado.get( estado.size()-1))) {
             case 3:
 
             case 5:
@@ -145,8 +170,8 @@ public class SyntheticAnalytic {
     }
     if(entrada=="EXP")
     {
-        // + get()
-        switch (estado.get()) {
+        // + get(estado.get( estado.size()-1))
+        switch (estado.get(estado.get( estado.size()-1))) {
             case 0:
 
             case 6:
@@ -163,8 +188,8 @@ public class SyntheticAnalytic {
     }
     if(entrada=="[")
     {
-        // + get()
-        switch (estado.get()) {
+        // + get(estado.get( estado.size()-1))
+        switch (estado.get(estado.get( estado.size()-1))) {
             case 4:
 
             case 5:
@@ -173,8 +198,8 @@ public class SyntheticAnalytic {
     }
     if(entrada=="]")
     {
-        // + get()
-        switch (estado.get()) {
+        // + get(estado.get( estado.size()-1))
+        switch (estado.get(estado.get( estado.size()-1))) {
             case 7:
 
             case 20:
@@ -184,8 +209,8 @@ public class SyntheticAnalytic {
         }
     }
     if(entrada=="("){
-        // + get()
-        switch(estado.get()){
+        // + get(estado.get( estado.size()-1))
+        switch(estado.get(estado.get( estado.size()-1))){
             case 0:
 
             case 8:
@@ -203,8 +228,8 @@ public class SyntheticAnalytic {
         }
     }
     if(entrada == ")"){
-        // + get()
-        switch(estado.get()){
+        // + get(estado.get( estado.size()-1))
+        switch(estado.get(estado.get( estado.size()-1))){
             case 5:
 
             case 6:
@@ -230,8 +255,8 @@ public class SyntheticAnalytic {
         }
     }
     if(entrada == "id"){
-        // + get()
-        switch(estado.get()){
+        // + get(estado.get( estado.size()-1))
+        switch(estado.get(estado.get( estado.size()-1))){
             case 0:
 
             case 6:
@@ -252,8 +277,8 @@ public class SyntheticAnalytic {
     }
     //vazio
     if(entrada == "$"){
-        // + get()
-        switch(estado.get()){
+        // + get(estado.get( estado.size()-1))
+        switch(estado.get(estado.get( estado.size()-1))){
             case 1:
 
             case 5:
@@ -278,8 +303,8 @@ public class SyntheticAnalytic {
     }
 
     if(entrada == "E"){
-        // + get()
-        switch(estado.get()){
+        // + get(estado.get( estado.size()-1))
+        switch(estado.get(estado.get( estado.size()-1))){
             case 1:
 
             case 6:
@@ -287,8 +312,8 @@ public class SyntheticAnalytic {
         }
     }
     if(entrada == "T"){
-        // + get()
-        switch(estado.get()){
+        // + get(estado.get( estado.size()-1))
+        switch(estado.get(estado.get( estado.size()-1))){
             case 1:
 
             case 6:
@@ -300,8 +325,8 @@ public class SyntheticAnalytic {
         }
     }
     if(entrada == "P"){
-        // + get()
-        switch(estado.get()){
+        // + get(estado.get( estado.size()-1))
+        switch(estado.get(estado.get( estado.size()-1))){
             case 1:
 
             case 6:
@@ -317,8 +342,8 @@ public class SyntheticAnalytic {
         }
     }
     if(entrada == "F"){
-        // + get()
-        switch(estado.get()){
+        // + get(estado.get( estado.size()-1))
+        switch(estado.get(estado.get( estado.size()-1))){
             case 1:
 
             case 6:
@@ -334,7 +359,7 @@ public class SyntheticAnalytic {
             case 12:
 
             case 13:
-            
+
         }
     }
 }
